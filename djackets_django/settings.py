3b68e1488975c287c7b076c1bab0bbaa62c27426
coshea20 <cormacoshea5@gmail.com>
2023-04-02 00:19:25 +0100

    Second Commit

--- djackets_django/settings.py
+++ djackets_django/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
