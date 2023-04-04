a431785dbfd48767fec8f3608e46b42dd89a1bad
coshea20 <cormacoshea5@gmail.com>
2023-04-05 00:32:49 +0100

    5/04/23 1st

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,19 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import Product from '../views/Product.vue'
+
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: Product
   }
 ]
 
--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="homeview">
-    <section class="hero is-medium is-dark mb-6">
+    <section class="hero is-medium is-dark mt-6">
       <div class="hero-body has-text-centered">
-        <p class="title mb-6">
+        <p class="title mt-6">
           Welcome to MotorMania
         </p>
         <p class="subtitle">
@@ -18,14 +18,14 @@
 
       <div class="column is-3" v-for="product in latestProducts" v-bind:key="product.id">
           <div class="box">
-            <figure class="image mb-4">
+            <figure class="image mt-4">
               <img :src="product.get_thumbnail">
             </figure>
 
             <h3 class="is-size-4">{{ product.name }}</h3>
             <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
-            View details
+            <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
             </div>
        </div>
     </div>
