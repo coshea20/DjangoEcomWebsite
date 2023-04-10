885f8836c7167710e5bf61ff044bd815c5c2afec
coshea20 <cormacoshea5@gmail.com>
2023-04-10 22:59:01 +0100

    10/04/23/2nd

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '../views/HomeView.vue'
 
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -19,6 +21,16 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
     path: '/:category_slug/:product_slug',
     name: 'Product',
     component: Product
@@ -27,7 +39,7 @@ const routes = [
     path: '/:category_slug',
     name: 'Category',
     component: Category
-  }
+  },
 ]
 
 const router = createRouter({
